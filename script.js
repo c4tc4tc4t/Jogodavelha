dc149c59c3729cb15af54e6f39d60faaac56c57e
c4tc4tc4t <emilano.ito@gmail.com>
2022-05-14 22:30:16 -0300

    refatoracao do css e JS/ quebrando td em função/ Lógica do vencedor dinamica/ Nome das variaveis com mais sentido/ Criar tabuleiro de forma mais simples

--- script.js
+++ script.js
@@ -1,196 +1,245 @@
-const miniCont = document.querySelector(".miniCont");
+const tamanhoTabuleiro = 3;
+function criacaoTabuleiro() {
+  const miniCont = document.querySelector(".miniCont");
 
-// miniCont.innerHTML =
-//   '<div class="linha"></div> <div class="linha"></div> <div class="linha"></div>';
-// const linha = document.querySelectorAll(".linha");
-
-// linha.forEach((item) => {
-//   item.innerHTML =
-//     '<div class="celula"></div><div class="celula"></div><div class="celula"></div>';
-// });
-
-const newDiv = [];
-for (i = 0; i < 3; i++) {
-  newDiv[i] = document.createElement("div");
-}
-newDiv.forEach((item) => {
-  item.classList.add("linha");
-  miniCont.appendChild(item);
-});
-
-const linha = document.querySelectorAll(".linha");
-const newDiv2 = [];
-for (i = 0; i < 3; i++) {
-  newDiv2[i] = document.createElement("div");
-}
-newDiv2.forEach((item) => {
-  item.classList.add("celula");
-  linha.forEach((i) => {
-    i.appendChild(item.cloneNode(true));
-  });
-});
-
-// linha.forEach((item) => {
-//   item.classList.add("celula");
-//   console.log(item);
-// });
-
-const a = document.querySelectorAll(".celula");
-const Arr = [...a];
-const Arr2 = [];
-
-const w1 = [];
-
-const x = Math.sqrt(Arr.length);
-
-let cont = 0;
-for (let i = 0; i < x; i++) {
-  for (let j = 0; j < x; j++) {
-    if (!Array.isArray(Arr2[i])) {
-      Arr2[i] = [];
-    }
-    Arr2[i][j] = Arr[cont];
-    cont++;
+  let divDasLinhas;
+  function criacaoDaLinha(divDasLinhas) {
+    divDasLinhas = document.createElement("div");
+    divDasLinhas.classList.add("linha");
+    miniCont.appendChild(divDasLinhas);
   }
+  function criacaoDaCelula(divDasCelulas) {
+    linhas.forEach((i) => {
+      divDasCelulas = document.createElement("div");
+      divDasCelulas.classList.add("celula");
+      i.appendChild(divDasCelulas);
+    });
+  }
+  for (i = 0; i < tamanhoTabuleiro; i++) {
+    criacaoDaLinha(divDasLinhas);
+  }
+
+  const linhas = document.querySelectorAll(".linha");
+  let divDasCelulas;
+  for (i = 0; i < tamanhoTabuleiro; i++) {
+    criacaoDaCelula(divDasCelulas);
+  }
+}
+criacaoTabuleiro();
+const celulas = document.querySelectorAll(".celula");
+const tabuleiroEmFormaDeArray = [...celulas];
+const tabuleiroEmFormaDeMatriz = [];
+
+const raizQuadradaDoTabuleiro = Math.sqrt(tabuleiroEmFormaDeArray.length);
+
+function ConverteArrayEmMatriz() {
+  function verificaSeEMatriz(index) {
+    if (!Array.isArray(tabuleiroEmFormaDeMatriz[index])) {
+      tabuleiroEmFormaDeMatriz[index] = [];
+    }
+  }
+  let contador = 0;
+  for (let i = 0; i < raizQuadradaDoTabuleiro; i++) {
+    for (let j = 0; j < raizQuadradaDoTabuleiro; j++) {
+      verificaSeEMatriz(i);
+      tabuleiroEmFormaDeMatriz[i][j] = tabuleiroEmFormaDeArray[contador];
+      contador++;
+    }
+  }
+}
+ConverteArrayEmMatriz();
+function criarRisco() {
+  let risco = document.createElement("div");
+  return risco;
+}
+function addItemNaDom(item, risco) {
+  item.appendChild(risco);
+  item.style.position = "relative";
+  elementoPaiDoTabuleiro.removeEventListener("click", onClick);
+}
+const teste = document.querySelector(".linha");
+console.log(teste.style.width);
+function tamanhoDoRisco(risco) {
+  risco.style.width = (40 * raizQuadradaDoTabuleiro).toString() + "px";
+}
+function tamanhoDoRiscoDiagonais(risco) {
+  risco.style.width = (tamanhoTabuleiro * 40 * Math.sqrt(2)).toString() + "px";
+  console.log(Math.sqrt(2));
 }
 
 function ganhouH(item) {
-  let risco = document.createElement("div");
-  risco.classList.add("riscoH");
-  item.appendChild(risco);
-  item.style.position = "relative";
-  function addAtivo() {
-    risco.classList.add("ativoH");
-  }
-  setTimeout(addAtivo, 10);
-  pai.removeEventListener("click", onClick);
+  let risco = criarRisco();
+  addItemNaDom(item, risco);
+  setTimeout(tamanhoDoRisco, 10, risco);
+  risco.classList.add("riscoH", "risco");
 }
 function ganhouV(item) {
-  let risco = document.createElement("div");
-  item.appendChild(risco);
-  item.style.position = "relative";
-  function addAtivo() {
-    risco.classList.add("ativoV");
-  }
-  setTimeout(addAtivo, 10);
-  risco.classList.add("riscoV");
-  pai.removeEventListener("click", onClick);
+  let risco = criarRisco();
+  addItemNaDom(item, risco);
+  setTimeout(tamanhoDoRisco, 10, risco);
+  risco.classList.add("riscoV", "risco");
 }
 function ganhouDP(item) {
-  let risco = document.createElement("div");
-  item.appendChild(risco);
-  item.style.position = "relative";
-  function addAtivo() {
-    risco.classList.add("ativoDP");
-  }
-  setTimeout(addAtivo, 10);
-  risco.classList.add("riscoDP");
-  pai.removeEventListener("click", onClick);
+  let risco = criarRisco();
+  addItemNaDom(item, risco);
+  setTimeout(tamanhoDoRiscoDiagonais, 10, risco);
+  risco.classList.add("riscoDP", "risco");
 }
 function ganhouDS(item) {
-  let risco = document.createElement("div");
-  item.appendChild(risco);
-  item.style.position = "relative";
-  function addAtivo() {
-    risco.classList.add("ativoDS");
-  }
-  setTimeout(addAtivo, 10);
-  risco.classList.add("riscoDS");
-  pai.removeEventListener("click", onClick);
+  let risco = criarRisco();
+  addItemNaDom(item, risco);
+  setTimeout(tamanhoDoRiscoDiagonais, 10, risco);
+  risco.classList.add("riscoDS", "risco");
 }
-// console.log(x);
-// console.log(Arr2[0][0].parentNode);
-let j = 0;
+
+let comparadorDeMarcacaoDeCampo = 0;
 function onClick(itemClasse) {
-  if (!itemClasse.target.classList.contains("celula")) {
+  function verificaCliqueNaCelula() {
+    if (!itemClasse.target.classList.contains("celula")) {
+      return true;
+    }
+    return false;
+  }
+  if (verificaCliqueNaCelula()) {
     return;
   }
-
-  if (
-    (itemClasse.target.innerText != "X") &
-    (itemClasse.target.innerText != "O")
-  ) {
-    if (j % 2 == 0) {
-      itemClasse.target.innerText = "X";
-    } else {
-      itemClasse.target.innerText = "O";
+  function adicionaElemento() {
+    if (
+      itemClasse.target.innerText != "X" &&
+      itemClasse.target.innerText != "O"
+    ) {
+      if (comparadorDeMarcacaoDeCampo % 2 == 0) {
+        itemClasse.target.innerText = "X";
+      } else {
+        itemClasse.target.innerText = "O";
+      }
+      ++comparadorDeMarcacaoDeCampo;
     }
-    ++j;
   }
+  adicionaElemento();
+  function verificaItensDasLinhas(arrayDeArmazenamento, linhaASerVerificada) {
+    if (arrayDeArmazenamento.every((x) => x === itemClasse.target.innerText)) {
+      ganhouH(linhaASerVerificada);
+    }
+  }
+  function verificaItensDasColunas(arrayDeArmazenamento, colunaASerVerificada) {
+    if (arrayDeArmazenamento.every((x) => x === itemClasse.target.innerText)) {
+      ganhouV(colunaASerVerificada);
+    }
+  }
+  function verificaItensDaDiagonalPrincipal(
+    arrayDeArmazenamento,
+    diagonalASerVerificada
+  ) {
+    if (arrayDeArmazenamento.every((x) => x === itemClasse.target.innerText)) {
+      ganhouDP(diagonalASerVerificada);
+    }
+  }
+  function verificaItensDaDiagonalSecundaria(
+    arrayDeArmazenamento,
+    diagonalASerVerificada
+  ) {
+    if (arrayDeArmazenamento.every((x) => x === itemClasse.target.innerText)) {
+      ganhouDS(diagonalASerVerificada);
+    }
+  }
+  function verificaSeGanhouLinhaColuna() {
+    for (let i = 0; i < tabuleiroEmFormaDeMatriz.length; i++) {
+      let arrayDeArmazenamentoLogicaVencedorLinha = [];
+      let arrayDeArmazenamentoLogicaVencedorColuna = [];
+      for (let j = 0; j < tabuleiroEmFormaDeMatriz.length; j++) {
+        arrayDeArmazenamentoLogicaVencedorLinha[j] =
+          tabuleiroEmFormaDeMatriz[i][j].innerText;
+        arrayDeArmazenamentoLogicaVencedorColuna[j] =
+          tabuleiroEmFormaDeMatriz[j][i].innerText;
 
-  for (let i = 0; i < Arr2.length; i++) {
-    let t = [];
-    let o = [];
-    for (let j = 0; j < Arr2.length; j++) {
-      t[j] = Arr2[i][j].innerText;
-      o[j] = Arr2[j][i].innerText;
-
-      if (j === Arr2.length - 1) {
-        if (t.every((x) => x === "X")) {
-          ganhouH(Arr2[i][0]);
-        } else if (t.every((x) => x === "O")) {
-          ganhouH(Arr2[i][0]);
-        } else if (o.every((x) => x === "X")) {
-          ganhouV(Arr2[0][i]);
-        } else if (o.every((x) => x === "O")) {
-          ganhouV(Arr2[0][i]);
+        if (j === tabuleiroEmFormaDeMatriz.length - 1) {
+          verificaItensDasLinhas(
+            arrayDeArmazenamentoLogicaVencedorLinha,
+            tabuleiroEmFormaDeMatriz[i][0]
+          );
+          verificaItensDasColunas(
+            arrayDeArmazenamentoLogicaVencedorColuna,
+            tabuleiroEmFormaDeMatriz[0][i]
+          );
         }
       }
     }
   }
-  const diagP = [];
-  const diagS = [];
-  cont = 0;
+  verificaSeGanhouLinhaColuna();
 
-  for (let m = 0; m < Arr2.length; m++) {
-    for (let n = 0; n < Arr2.length; n++) {
-      if (m == n) {
-        diagP[cont] = Arr2[m][n].innerText;
-        cont++;
+  function verificaSeGanhouDiagonais() {
+    const diagonalPrincipal = [];
+    const diagonalSecundaria = [];
+    contador = 0;
+    function pegarElementosDaDiagonalPrincipal() {
+      for (let m = 0; m < tabuleiroEmFormaDeMatriz.length; m++) {
+        for (let n = 0; n < tabuleiroEmFormaDeMatriz.length; n++) {
+          if (m == n) {
+            diagonalPrincipal[contador] =
+              tabuleiroEmFormaDeMatriz[m][n].innerText;
+            contador++;
+          }
+        }
       }
     }
+    pegarElementosDaDiagonalPrincipal();
+    contador = 0;
+    function pegarElementosDaDiagonalSecundaria() {
+      let coluna = tabuleiroEmFormaDeMatriz.length - 1;
+      for (let i = 0; i < tabuleiroEmFormaDeMatriz.length; i++) {
+        diagonalSecundaria[contador] =
+          tabuleiroEmFormaDeMatriz[i][coluna].innerText;
+        contador++;
+        --coluna;
+      }
+    }
+    pegarElementosDaDiagonalSecundaria();
+    verificaItensDaDiagonalPrincipal(
+      diagonalPrincipal,
+      tabuleiroEmFormaDeMatriz[0][0]
+    );
+    verificaItensDaDiagonalSecundaria(
+      diagonalSecundaria,
+      tabuleiroEmFormaDeMatriz[0][tabuleiroEmFormaDeMatriz.length - 1]
+    );
   }
-
-  cont = 0;
-  let coluna = Arr2.length - 1;
-  for (let i = 0; i < Arr2.length; i++) {
-    diagS[cont] = Arr2[i][coluna].innerText;
-    cont++;
-    --coluna;
-  }
-  if (diagP.every((x) => x === "X")) {
-    ganhouDP(Arr2[0][0]);
-  } else if (diagP.every((x) => x === "O")) {
-    ganhouDP(Arr2[0][0]);
-  } else if (diagS.every((x) => x === "X")) {
-    ganhouDS(Arr2[0][2]);
-  } else if (diagS.every((x) => x === "O")) {
-    ganhouDS(Arr2[0][2]);
-  }
+  verificaSeGanhouDiagonais();
 }
 
-const pai = document.querySelector(".miniCont");
-pai.addEventListener("click", onClick);
-
+const elementoPaiDoTabuleiro = document.querySelector(".miniCont");
+function addOnClickNoElementoPai() {
+  elementoPaiDoTabuleiro.addEventListener("click", onClick);
+}
+addOnClickNoElementoPai();
 function reset() {
-  Arr.forEach((item) => {
-    item.innerText = "";
-  });
-  const linha = document.querySelectorAll(".riscoH");
-  const coluna = document.querySelectorAll(".riscoV");
-  linha.forEach((item) => {
-    item.classList.remove("riscoH");
-    item.classList.remove("ativo");
-  });
-  coluna.forEach((item) => {
-    item.classList.remove("riscoV");
-    item.classList.remove("ativo");
-  });
-  j = 0;
-  pai.addEventListener("click", onClick);
+  function esvaziaElementosDoTabuleiro() {
+    tabuleiroEmFormaDeArray.forEach((item) => {
+      item.innerText = "";
+    });
+  }
+  esvaziaElementosDoTabuleiro();
+  function removeAsClassesDosElementos() {
+    const linhas = document.querySelectorAll(".riscoH");
+    const colunas = document.querySelectorAll(".riscoV");
+    linhas.forEach((item) => {
+      item.classList.remove("riscoH");
+      item.classList.remove("ativo");
+    });
+    colunas.forEach((item) => {
+      item.classList.remove("riscoV");
+      item.classList.remove("ativo");
+    });
+  }
+  function voltandoOMarcadorParaX() {
+    comparadorDeMarcacaoDeCampo = 0;
+  }
+  voltandoOMarcadorParaX();
+  removeAsClassesDosElementos();
+  addOnClickNoElementoPai();
 }
-const botao = document.querySelectorAll(".botaoJgrDnv");
-botao.forEach((item) => {
-  item.addEventListener("click", reset);
-});
-// const botao = document.querySelectorAll(".botaoJgrDnv");
+function addResetNoBotao() {
+  const botao = document.querySelector(".botaoJgrDnv");
+  botao.addEventListener("click", reset);
+}
+addResetNoBotao();
